03c35f3f7125b57c769130c65463f6169edb930a
Sebastian Schmitt <mulenker@gmail.com>
2022-01-08 00:42:44 +0100

    enhanced versioning

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+from platform import version
 from conans import ConanFile
 from conans import tools
 from conan.tools.cmake import CMakeToolchain, CMake
@@ -6,7 +7,6 @@ from conan.tools.layout import cmake_layout
 
 class PlutovgConan(ConanFile):
     name = "plutovg"
-    version = "0.1.0"
     license = "MIT"
     author = "tschabo"
     url = ""
@@ -21,7 +21,7 @@ class PlutovgConan(ConanFile):
             del self.options.fPIC
     
     def source(self):
-        tools.get("https://github.com/tschabo/plutovg/archive/refs/tags/v0.1.0.tar.gz", md5='eea1ae4ae376241ed422210a39c57ff2', sha1='29da1df82175af272be2c24a4f845482508762b8', sha256='c55082d88b69e0d647df2f3dcfe2ed3ec8cea9178e8257b87db7434197d5f9e7')
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True)
 
     def layout(self):
         cmake_layout(self)
@@ -32,7 +32,7 @@ class PlutovgConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_script_folder="plutovg-0.1.0")
+        cmake.configure()
         cmake.build()
 
     def package(self):
